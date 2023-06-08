8121e6871b13f7ce81a6342e937e46bfa02c2282
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-06-08 17:59:42 +0300

    initialized project

---
+++ sber-dex/src/app/page.js
@@ -0,0 +1,10 @@
+import styles from './styles/page.module.css'
+
+export default function Home() {
+  return (
+      <main>
+        <h1 style={{color: 'pink'}}>I am not Uniswap</h1>
+        <h1 style={{color: '#008000'}}>Sorry mamma Imma SberDex</h1>
+      </main>
+  )
+}
