4fd87877d1b81aaed611deb5be87b170849ad424
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-06-08 19:24:20 +0300

    added footer

---
+++ sber-dex/src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import styles from "./footer.module.css";
+import Image from "next/image";
+import logo from "../../../public/Icons/MZKb.png"
+
+const Footer = () => {
+    return ( 
+        <div className={styles.mainContainer}>
+                <Image src={logo} width={128} height={110}/>
+                <p className={styles.footerText}>SBERDEX by MetaZK</p>
+                <p className={styles.footerText}>2023</p>
+        </div>
+     );
+}
+ 
+export default Footer;
