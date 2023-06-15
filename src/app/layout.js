d08881532cbcc51ab12a1d0faf74d8c9fdaae9aa
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-06-15 21:55:51 +0300

    added themeProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import './styles/globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/footer/footer'
 import { Providers } from '../store/provide'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
--- src/store/provide.js
+++ src/store/provide.js
@@ -2,11 +2,15 @@
 
 import { Provider } from "react-redux";
 import {store} from "./store";
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import original from 'react95/dist/themes/original';
 
 export function Providers({children}){
     return(
         <Provider store={store}>
+            <ThemeProvider theme={original}>
             {children}
+            </ThemeProvider>
         </Provider>
     )
 }
