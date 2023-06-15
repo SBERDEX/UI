c6996a5b18b4008b7af2acca0909f549af574a5e
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-06-15 22:20:44 +0300

    updated webpack

--- next.config.js
+++ next.config.js
@@ -2,7 +2,46 @@
 const nextConfig = {
     env: {
         networkName: "goerli",
-    }
+    },
+    rules: [
+        // Другие правила...
+        
+        {
+          test: /\.woff2$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        }
+      ]
+      
 }
 
 module.exports = nextConfig
+
+module.exports = {
+    // Другие настройки Next.js...
+  
+    webpack: (config, { dev, isServer }) => {
+      // Другие настройки Webpack...
+  
+      // Настройки для загрузчиков
+      config.module.rules.push({
+        test: /\.woff2$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }
+      });
+  
+      // Вернуть измененную конфигурацию Webpack
+      return config;
+    }
+  };
+  
