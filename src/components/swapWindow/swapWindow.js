56f5764ea5b3f7ff075ebad09ad5b19491373401
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-06-26 16:36:34 +0300

    test

--- src/components/swapWindow/swapWindow.js
+++ src/components/swapWindow/swapWindow.js
@@ -1,7 +1,9 @@
 'use client';
 import styles from "./swapWindow.module.css"
-import { TextInput, Select } from "react95";
+import { TextInput, Select, Button } from "react95";
 import styled from "styled-components";
+import Image from "next/image";
+import switchIcon from "../../../public/Icons/switch.png";
 
 const StyledInput = styled(TextInput)`
     height: 50px;
@@ -10,6 +12,7 @@ const StyledInput = styled(TextInput)`
     font-weight: 400;
     font-size: 24px;
     line-height: 24px;
+    margin-bottom: 15px;
 `
 
 const StyledSelect = styled(Select)`
@@ -22,7 +25,15 @@ const StyledSelect = styled(Select)`
     color: #000000;
 `
 
+const SwitchButton = styled(Button)`
+    height: 50px;
+    width: 40px;
+    margin-bottom: 15px;
+    margin-left: 170px;
+`
+
 const SwapWindow = () => {
+
     const currencies = [
         { value: "usdt", label: "USDT" },
         { value: "eth", label: "ETH" }
@@ -31,18 +42,34 @@ const SwapWindow = () => {
     return (
         <>
             <div className={styles.mainContainer}>
+
                 <div className={styles.header}>
                     <p className={styles.headerText}>Swap</p>
                 </div>
 
                 <div className={styles.body}>
+
                     <div className={styles.inputContainer}>
                         <StyledInput fullWidth type="number"  placeholder="Type here..."/>
-                        <StyledSelect width={110} options={currencies} />
+                        <StyledSelect defaultValue={0} width={110} options={currencies} />
                     </div>
+
+
+                    <SwitchButton>
+                        <Image src={switchIcon} width={31} height={32} alt="Switch icon" />
+                    </SwitchButton>
+
+                    <div className={styles.inputContainer}>
+                        <StyledInput fullWidth type="number"  placeholder="Type here..."/>
+                        <StyledSelect defaultValue={1}  width={110} options={currencies} />
+                    </div>
+
+                    <div className={styles.rate}>
+                        <p className={styles.rateText}>{"1"} {"USDT"} = {"80"} {"RUBT"}</p>
+                    </div>
+
                 </div>
 
-                
             </div>
         </>
     );
