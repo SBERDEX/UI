5de7fcfbe70b5ee6e2d22d6fbeb8c824423b0f15
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-06-30 18:10:06 +0300

    TODO fix footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,13 +4,14 @@ import Image from "next/image";
 import { useSelector } from "react-redux";
 
 const Footer = () => {
-    const currentAccount = useSelector(state => state.account.account);
+    const walletStatus = useSelector(state => state.isConnected.isConnected);
+
 
     return ( 
         <div className={styles.mainContainer}>
                 <p className={styles.footerText}>SBERDEX by MetaZK</p>
                 <div className={styles.walletStatus}>
-                    {currentAccount.length > 0 ? <p className={styles.walletStatusText}>Wallet Connected</p> : <p className={styles.walletStatusText}>Wallet is not Connected</p>}
+                    {walletStatus ? <p className={styles.walletStatusText}>Wallet Connected</p> : <p className={styles.walletStatusText}>Wallet is not Connected</p>}
                     
                 </div>
         </div>
