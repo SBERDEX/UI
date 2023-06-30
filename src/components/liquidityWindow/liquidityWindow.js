8ce6ca214297044ba1a974cb9493305a7a7de80e
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-06-30 17:01:58 +0300

    fixed margins in price range sections

--- src/components/liquidityWindow/liquidityWindow.js
+++ src/components/liquidityWindow/liquidityWindow.js
@@ -24,16 +24,14 @@ const StyledSelect = styled(Select)`
 `
 
 const StyledNumberInput = styled(NumberInput)`
-    height: 50px;
     font-family: 'Grotesque';
     font-style: normal;
     font-weight: 400;
-    font-size: 24px;
-    line-height: 24px;
+    font-size: 32px;
+    line-height: 32px;
 `
 
 const StyledButton = styled(Button)`
-    
     font-family: 'Grotesque';
     font-style: normal;
     font-weight: 400;
