b34a79ccba0094e17451735961269475562829bc
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-07-22 17:28:33 +0300

    fixed global header and global footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,19 +2,26 @@
 import styles from "./footer.module.css";
 import Image from "next/image";
 import { useSelector } from "react-redux";
+import { useRouter } from "next/navigation";
 
 const Footer = () => {
     const walletStatus = useSelector(state => state.isConnected.isConnected);
+    const router = useRouter();
+    const currentPage = router.pathname;
 
 
     return ( 
-        <div className={styles.mainContainer}>
+            <>
+            {currentPage && (                               
+                 <div className={styles.mainContainer}>
                 <p className={styles.footerText}>SBERDEX by MetaZK</p>
                 <div className={styles.walletStatus}>
                     {walletStatus ? <p className={styles.walletStatusText}>Wallet Connected</p> : <p className={styles.walletStatusText}>Wallet is not Connected</p>}
                     
                 </div>
-        </div>
+                </div>)}
+
+            </>
      );
 }
  
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,28 +1,39 @@
+'use client'
 import WalletConnect from "../wallet/walletCnnect";
 import swapIcon from "../../../public/Icons/SwapIcon.png";
 import liqIcon from "../../../public/Icons/LiqRubIcon.png";
 import Link from "next/link";
 import styles from "./header.module.css";
 import Image from "next/image";
+import { useRouter } from 'next/navigation';
 
 
 const Header = () => {
-    return ( 
-    <>
-        <div className={styles.mainContainer}>
-            <div className={styles.linkContainer}>
-                <Link href="/liquidity">
-                        <Image src={liqIcon} height={126} width={126} alt="liquidity icon"/>
-                        <p className={styles.liqText}>Liquidity</p>
-                </Link>
+    const router = useRouter();
+    const currentPage = router.pathname;
+    console.log(currentPage)
 
-                <Link href="/swap">
-                        <Image src={swapIcon} height={120} width={120} alt="swap icon"/>
-                        <p className={styles.swapText}>Swap</p>
-                </Link>
+    return ( 
+    <> 
+        {
+            currentPage && (
+                <div className={styles.mainContainer}>
+                <div className={styles.linkContainer}>
+                    <Link href="/liquidity">
+                            <Image src={liqIcon} height={126} width={126} alt="liquidity icon"/>
+                            <p className={styles.liqText}>Liquidity</p>
+                    </Link>
+    
+                    <Link href="/swap">
+                            <Image src={swapIcon} height={120} width={120} alt="swap icon"/>
+                            <p className={styles.swapText}>Swap</p>
+                    </Link>
+                </div>
+                <WalletConnect />
             </div>
-            <WalletConnect />
-        </div>
+            )
+        }
+
     </> 
     );
 }
